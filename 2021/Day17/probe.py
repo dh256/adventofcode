86ecabe55373603878a6da4a3de0ebe856993fb8
David Hanley <david.hanley12@btinternet.com>
2021-12-17 13:12:02 +0000

    Comments added

--- 2021/Day17/probe.py
+++ 2021/Day17/probe.py
@@ -1,4 +1,4 @@
-# Assumption, x range is +ve and y range is -ve
+# Assumption, target lies to bottom left of start point i.e. x range is +ve and y range is -ve
 import re
 from collections import namedtuple
 
@@ -14,8 +14,8 @@ class Probe:
     def highest_y(self):
         i_vels_hit_target = set()
         highest_ever_y = 0
-        for ix_vel in range(1,self.x_range.end+1):                        # what is the range of initial x_vel - if outside a certain range will never hit target?
-            for iy_vel in range(self.y_range.start,self.x_range.end+1):   # what is the range of initial y_vel - if outside a certain range will never hit target?
+        for ix_vel in range(1,self.x_range.end+1):   # x_vel must be greater than 1 to move forward and <= x_range end to not overshoot target on first move 
+            for iy_vel in range(self.y_range.start,self.x_range.end+1):   # y_vel must be greater than y_range start to not overshoot on first move - if outside a certain range will never hit target, this is just an arbitrary no just now. Should be able to calculate this
                 highest_y = 0
                 x, y = 0, 0
                 x_vel, y_vel = ix_vel, iy_vel    
