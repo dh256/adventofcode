144e0617ba4a845581239f7302f7bdc879a67d16
David Hanley <david.hanley12@btinternet.com>
2022-09-12 12:25:54 +0100

    Day 5 Parts 1 and 2 Minor change

--- 2017/Day5/day5.py
+++ 2017/Day5/day5.py
@@ -2,5 +2,7 @@ from CPU import CPU
 
 cpu = CPU('input.txt')
 print(f'{cpu.process()}')
+
+# reload machine instructions - they were changed above
 cpu = CPU('input.txt')
 print(f'{cpu.process2()}')
