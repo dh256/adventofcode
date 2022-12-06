5aeff7180774162d410a0a36747770f37f791b30
David Hanley <david.hanley12@btinternet.com>
2022-12-06 12:12:27 +0000

    Parts 1 and 2

--- 2022/Day6/Device.py
+++ 2022/Day6/Device.py
@@ -7,6 +7,8 @@ class Device:
     def first_marker(self,chars) -> int:
         '''
         Returns position of first marker
+        That is the end position of first string of length chars that contains unique
+        characters in the input stream
         '''
         for index in range(0,len(self.stream)-chars-1):
             if len(set(self.stream[index:index+chars])) == chars:
