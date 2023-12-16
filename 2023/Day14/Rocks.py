025126a292a54bc513949b3e5f76f95d853b4ca8
David Hanley <david.hanley12@btinternet.com>
2023-12-16 18:30:42 +0000

    Day 14 Part 2 - got it but not sure why

--- 2023/Day14/Rocks.py
+++ 2023/Day14/Rocks.py
@@ -45,14 +45,43 @@ class Rocks:
             for r in rocks_to_move:
                 self.round_rocks[r[0]] = Point(col,start_row+round_offset)
                 round_offset += 1
+
+    def move_rocks_S(self, col, start_row, end_row, round_rocks) -> int:
+        # There must be at least two spaces between cube rocks
+        if end_row-start_row >= 2:
+            # move rocks
+            rocks_to_move = list(filter(lambda r: start_row <= r[1] <= end_row, round_rocks))
+            round_offset = 0
+            for r in rocks_to_move:
+                self.round_rocks[r[0]] = Point(col,(end_row-1)-round_offset)
+                round_offset += 1
+
+    def move_rocks_W(self, row, start_col, end_col, round_rocks) -> int:
+        # There must be at least two spaces between cube rocks
+        if end_col-start_col >= 2:
+            # move rocks
+            rocks_to_move = list(filter(lambda r: start_col <= r[1] <= end_col, round_rocks))
+            round_offset = 0
+            for r in rocks_to_move:
+                self.round_rocks[r[0]] = Point(start_col+round_offset,row)
+                round_offset += 1
+
+    def move_rocks_E(self, row, start_col, end_col, round_rocks) -> int:
+        # There must be at least two spaces between cube rocks
+        if end_col-start_col >= 2:
+            # move rocks
+            rocks_to_move = list(filter(lambda r: start_col <= r[1] <= end_col, round_rocks))
+            round_offset = 0
+            for r in rocks_to_move:
+                self.round_rocks[r[0]] = Point((end_col-1)-round_offset,row)
+                round_offset += 1
             
 
     def tilt(self, direction: str) -> None:
         ''' 
         Tilt North and calculate total load on North beams
         '''
-        if direction == 'N':    
-
+        if direction == 'N':   
             for col in range(self.width):
                 round_rocks = [(i[0], i[1].y) for i in filter(lambda r: r[1].x == col, self.round_rocks.items())]
                 start_row = 0
@@ -65,6 +94,46 @@ class Rocks:
                 if end_row is None or end_row < self.height:
                     self.move_rocks_N(col,start_row,self.height,round_rocks)
 
+        elif direction == 'S':    
+            for col in range(self.width):
+                round_rocks = [(i[0], i[1].y) for i in filter(lambda r: r[1].x == col, self.round_rocks.items())]
+                start_row = 0
+                end_row = None
+                for end_row in self.cube_rocks_col[col]:
+                    self.move_rocks_S(col,start_row,end_row,round_rocks)             
+                    start_row = end_row + 1
+
+                # move everything else down:
+                if end_row is None or end_row < self.height:
+                    self.move_rocks_S(col,start_row,self.height,round_rocks)
+
+
+        elif direction == 'W':    
+            for row in range(self.height):
+                round_rocks = [(i[0], i[1].x) for i in filter(lambda r: r[1].y == row, self.round_rocks.items())]
+                start_col = 0
+                end_col = None
+                for end_col in self.cube_rocks_row[row]:
+                    self.move_rocks_W(row,start_col,end_col,round_rocks)             
+                    start_col = end_col + 1
+
+                # move everything else up:
+                if end_col is None or end_col < self.width:
+                    self.move_rocks_W(row,start_col,self.width,round_rocks)
+
+        elif direction == 'E':    
+            for row in range(self.height):
+                round_rocks = [(i[0], i[1].x) for i in filter(lambda r: r[1].y == row, self.round_rocks.items())]
+                start_col = 0
+                end_col = None
+                for end_col in self.cube_rocks_row[row]:
+                    self.move_rocks_E(row,start_col,end_col,round_rocks)             
+                    start_col = end_col + 1
+
+                # move everything else up:
+                if end_col is None or end_col < self.width:
+                    self.move_rocks_E(row,start_col,self.width,round_rocks)
+
 
     def total_positions(self) -> int:
         total = 0
@@ -74,6 +143,10 @@ class Rocks:
         return total
 
     def part1(self) -> int:
-        self.tilt('N')
-        total = self.total_positions()
-        return total       
+        for x in range (200):
+            self.tilt('N')
+            self.tilt('W')
+            self.tilt('S')
+            self.tilt('E')
+            print(f'{x},{self.total_positions()}')
+        return 0       
--- 2023/Day14/main.py
+++ 2023/Day14/main.py
@@ -1,5 +1,10 @@
 '''
 Part 1: Complete - but it's going to be too slow - must be another method
+
+Part 2: Added code to do W, E, S tilts but too slow to run 1000000000 times.
+Noticed during testing that after a few runs, a repeating pattern of 7 numbers stars to emerge.
+Same thing happenned with real input. Summised it must be one of the repeating 7 numbers.
+Not sure why this happens though.
 '''
 
 from Rocks import Rocks
