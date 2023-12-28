1145da7bb7956bc92849e45b80bf02ac85fd743d
David Hanley <david.hanley12@btinternet.com>
2023-12-28 14:47:19 +0000

    Code tidy up

--- 2023/Day23/Hike.py
+++ 2023/Day23/Hike.py
@@ -74,68 +74,14 @@ class Hike:
                     self.path(p, steps+1, next_visited,ignore_paths)
                 return  
 
-    def part1_alt(self) -> int:
+    def part1(self) -> int:
         self.path_lengths = set()
         self.path(self.start, 0, set(), False)
         return max(self.path_lengths)
     
-    def part2_alt(self) -> int:
+    def part2(self) -> int:
         self.path_lengths = set()
         self.path(self.start, 0, set(), True)
         return max(self.path_lengths)
 
-    def part1(self, ignore_slopes: bool = False) -> int:
-        '''
-        Calculate longest hike you can take
-        If ignore_slopes is True treat all slopes as normal paths
-        '''
-        path_lengths: list[int] = list()
-        q = deque()
-        q.append((self.start,0,set()))      # point, direction, steps, visited
-        while len(q) > 0:
-            curr_point, steps, visited = q.pop()
-            
-            # if a slope can only move in direction of that slope
-            if not ignore_slopes:
-                if self.grid[curr_point.y][curr_point.x] in Hike.slopes.keys():
-                    offset_range = [Hike.offsets[self.slopes[self.grid[curr_point.y][curr_point.x]]]]
-                else:
-                    # otherwise check all directions
-                    offset_range = Hike.offsets.values()
-            else:
-                offset_range = Hike.offsets.values()
-
-            # check all possible next points
-            for offset in offset_range:
-                next_point = curr_point.offset(offset)
-
-                # is on grid
-                if not self.on_grid(next_point):
-                    continue
-
-                # already visited
-                if next_point in visited:
-                    continue
-
-                # reached the end
-                if next_point == self.end:
-                    # reached end, append to path lengths
-                    print(f'End Reached: Path length {steps+1}')
-                    path_lengths.append(steps+1)
-                    continue
-
-                # if a path or a slope - add next point to Q
-                if self.grid[next_point.y][next_point.x] == '.' or self.grid[next_point.y][next_point.x] in Hike.slopes.keys():
-                    new_visited = {v for v in visited}
-                    new_visited.add(curr_point)
-                    q.append((next_point,steps+1,new_visited))  
-                    continue 
-
-        return max(path_lengths)
     
-    def part2(self) -> int:
-        '''
-        Calculate longest hike you can take
-        This time treat all slopes as normal paths
-        '''
-        return self.part1(True)
--- 2023/Day23/tests/test.py
+++ 2023/Day23/tests/test.py
@@ -7,12 +7,12 @@ test_data2=[('tests/input.txt',154)]
 @pytest.mark.parametrize('file_name,result',test_data)
 def test_part1(file_name,result):
     hike = Hike(file_name)
-    assert(hike.part1_alt() == result)
+    assert(hike.part1() == result)
 
 @pytest.mark.parametrize('file_name,result',test_data2)
 def test_part2(file_name,result):
     hike = Hike(file_name)
-    assert(hike.part2_alt() == result)
+    assert(hike.part2() == result)
 
     
 
