5981cbaed1ccab854b500c528189a08a56df4b2e
David Hanley <david.hanley12@btinternet.com>
2024-11-18 16:40:46 +0000

    Minor update

--- 2015/Day15/Day15.py
+++ 2015/Day15/Day15.py
@@ -36,23 +36,22 @@ class Day15:
     def solution(self) -> tuple[int,int]:
         result_part1: int = 0
         result_part2: int = 0
-        for c in product(range(0,101),repeat=len(self.ingredients)):
-            if sum(c) == 100:                   # only interested in those that add up to exactly 100
-                capacity: int = 0 
-                durability: int = 0
-                flavour: int = 0
-                texture: int = 0
-                calories: int = 0
-                for i in range(0,len(self.ingredients)):
-                    capacity += self.ingredients[i].capacity * c[i]
-                    durability += self.ingredients[i].durability * c[i]
-                    flavour += self.ingredients[i].flavour * c[i]
-                    texture += self.ingredients[i].texture * c[i]
-                    calories += self.ingredients[i].calories * c[i]
-                
-                new_result: int = (capacity if capacity >= 0 else 0) * (durability if durability >= 0 else 0) * (flavour if flavour >= 0 else 0) * (texture if texture >= 0 else 0)
-                result_part1 = new_result if new_result > result_part1 else result_part1
-                result_part2 = new_result if calories == 500 and new_result > result_part2 else result_part2
+        for c in [c for c in product(range(0,101),repeat=len(self.ingredients)) if sum(c) == 100]:
+            capacity: int = 0 
+            durability: int = 0
+            flavour: int = 0
+            texture: int = 0
+            calories: int = 0
+            for i in range(0,len(self.ingredients)):
+                capacity += self.ingredients[i].capacity * c[i]
+                durability += self.ingredients[i].durability * c[i]
+                flavour += self.ingredients[i].flavour * c[i]
+                texture += self.ingredients[i].texture * c[i]
+                calories += self.ingredients[i].calories * c[i]
+            
+            new_result: int = (capacity if capacity >= 0 else 0) * (durability if durability >= 0 else 0) * (flavour if flavour >= 0 else 0) * (texture if texture >= 0 else 0)
+            result_part1 = new_result if new_result > result_part1 else result_part1
+            result_part2 = new_result if calories == 500 and new_result > result_part2 else result_part2
               
         return (result_part1, result_part2)      
                 
