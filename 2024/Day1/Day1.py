a4e40c1f4a1aab37b57b49cb83aa869a1ac94802
David Hanley <david.hanley12@btinternet.com>
2024-12-01 08:38:42 +0000

    Type annotation

--- 2024/Day1/Day1.py
+++ 2024/Day1/Day1.py
@@ -14,14 +14,14 @@ class Day1:
         self.col2: list[int] = sorted([int(num[1]) for num in num_pairs])
             
     def part1(self) -> int:
-        pairs = zip(self.col1,self.col2)
-        distance = 0
+        pairs: list[tuple[int,int]] = zip(self.col1,self.col2)
+        distance: int = 0
         for pair in pairs:
             distance += abs(pair[0] - pair[1])
         return distance
 
     def part2(self) -> int:
-        sim_score = 0
+        sim_score: int = 0
         for num in self.col1:
             sim_score += num * len(list(filter(lambda c2: c2 == num, self.col2)))
         return sim_score             
