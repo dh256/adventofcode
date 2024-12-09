4185990cd0c553c7cf96fc332ced8d1a4fb0f2af
David Hanley <david.hanley12@btinternet.com>
2024-12-09 17:20:00 +0000

    Added crude execution time calculation

--- 2024/Day8/day_8.py
+++ 2024/Day8/day_8.py
@@ -3,11 +3,17 @@
 David Hanley, December 2024
 '''
 from Day8 import Day8
+from time import time
                             
 def main():
     d = Day8('input.txt')
+    start_time = time()
     print(f'Part 1: {d.part1()}')
+    print(f'Execution Time: {(time()-start_time):.3f}')
+
+    start_time = time()
     print(f'Part 2: {d.part2()}')
+    print(f'Execution Time: {(time()-start_time):.3f}')
 
 if __name__ == '__main__':
     main()
