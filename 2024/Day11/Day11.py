733dd09006d2e22116c9fc67a27c9854d17e1388
David Hanley <david.hanley12@btinternet.com>
2024-12-11 22:46:34 +0000

    Tidy Up

--- 2024/Day11/Day11.py
+++ 2024/Day11/Day11.py
@@ -14,26 +14,27 @@ class Day11:
             nums_generated += self.generate_stone_nums(num,0)
         return nums_generated
     
-    # functools cache method makes this insanely fast as it caches results from calls with same inputs
     # dynamic programming
+    # functools cache method makes this insanely fast as it caches results from calls with same inputs
     @functools.cache
     def generate_stone_nums(self,num,blinks):
-        # reached blink 75, return single number
+        
+        # reached required number of blinks, no need to recurse any further. Return count of 1 (this num)
         if blinks == self.max_blinks:
             return 1
         
-        # recursively, work out next numbers (according to rules) and count number generated
+        # Work out next numbers (according to rules) and add the count of this to number generated
         nums_generated = 0
         if num == 0:
             nums_generated += self.generate_stone_nums(1,blinks+1)
         elif len(str(num)) % 2 == 0:
             pow10 = pow(10, len(f'{num}') // 2)         # would be great if there was an easy way to do this with maths
-            left_num = num // pow10
-            right_num = num % pow10
-            nums_generated += self.generate_stone_nums(left_num,blinks+1)
-            nums_generated += self.generate_stone_nums(right_num,blinks+1)
+            nums_generated += self.generate_stone_nums(num // pow10,blinks+1)
+            nums_generated += self.generate_stone_nums(num % pow10,blinks+1)
         else:
             nums_generated += self.generate_stone_nums(num * 2024, blinks+1)
+        
+        # return count of numbers generated
         return nums_generated
     
     
