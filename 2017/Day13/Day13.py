a10abc1768f23e98496c3c5d4ee50850ff61b9e6
David Hanley <dh256@users.noreply.github.com>
2025-11-19 12:24:41 +0000

    2017 Day 13 Part 2

--- 2017/Day13/Day13.py
+++ 2017/Day13/Day13.py
@@ -1,6 +1,5 @@
-import re
 from dataclasses import dataclass
-
+from copy import deepcopy
 @dataclass
 class Layer:
     range: int
@@ -14,7 +13,9 @@ class Layer:
         elif self.scanner_pos == self.range-1:
             self.scanner_increment = -1
         
-
+    def reset(self):
+        self.scanner_pos = 0
+        self.scanner_increment = 1
 class Day13:
 
     def __init__(self, file_name) -> None:
@@ -30,24 +31,54 @@ class Day13:
             self._layers[layer_depth] = Layer(layer_range)
         self._max_depth = layer_depth
     
+    def reset(self) -> None:
+        for k in self._layers.keys():
+            self._layers[k].reset()
+    
+    def _caught_layers(self,part: int) -> list[int]:
+        '''
+        Returns list of depth of layers where packet is caught
+        '''
+        caught_layers: list[int] = list()
+        for curr_packet_depth in range(0,self._max_depth+1):
+            for depth in self._layers.keys():
+                # check whether it's a catch
+                if depth == curr_packet_depth and self._layers[depth].scanner_pos == 0:
+                    if part == 1:
+                        caught_layers.append(depth)
+                    else:
+                        return [0]
+                    
+                # move scanner and if necessary change increment
+                self._layers[depth].move_scanner()
+        
+        return caught_layers
+    
     def part1(self) -> int:
         '''
         Move packet through all layers and record any layers it is caught in
         When complete sum up the depth*range of any caught layer
         '''
-        caught_layers: int = 0
-        for curr_packet_depth in range(0,self._max_depth+1):
-            for depth in self._layers.keys():
-                # check whether it's a catch
-                if depth == curr_packet_depth and self._layers[depth].scanner_pos == 0:
-                    caught_layers += depth * self._layers[depth].range
-                    
-                # move scanner and if necessary change increment
-                self._layers[depth].move_scanner()
+        caught_layers: list[int] = self._caught_layers(1)
+        return sum(map(lambda d: d * self._layers[d].range, caught_layers))
         
-        return caught_layers 
-        
-
     def part2(self) -> int:
-        pass
+        '''
+        Although works is slow. There is a more efficient method
+        '''
+        depth: int = 0
+        while True:
+            if depth % 10000 == 0:
+                print(depth)
+            
+            for k in self._layers.keys():
+                self._layers[k].move_scanner()
+            
+            state: dict[int, Layer] = deepcopy(self._layers)
+            
+            if len(self._caught_layers(2)) == 0:
+                return depth + 1
+            
+            self._layers = state
+            depth += 1
             
--- 2017/Day13/tests/test_day13.py
+++ 2017/Day13/tests/test_day13.py
@@ -2,11 +2,14 @@ import pytest
 from Day13 import Day13
 
 test_data=[('tests/test1.txt',24)]
-
+test_data2=[('tests/test1.txt',10)]
 
 @pytest.mark.parametrize('file_name,result',test_data)
 def tests_part1(file_name,result):
     day13 = Day13(file_name)
     assert(day13.part1() == result)
     
-
+@pytest.mark.parametrize('file_name,result',test_data2)
+def tests_part2(file_name,result):
+    day13 = Day13(file_name)
+    assert(day13.part2() == result)
