c72f9b2bebdc299a2219e3a175efb953a2daeade
David Hanley <dh256@users.noreply.github.com>
2025-12-10 10:45:22 +0000

    Day 10 Parts 1 and 2

--- 2025/day10/Day10.py
+++ 2025/day10/Day10.py
@@ -1,16 +1,55 @@
+from dataclasses import dataclass
+from itertools import combinations
+from scipy.optimize import linprog
 
-# add parent directory to path for imports
-import sys
-sys.path.insert(0,'..')
+@dataclass
+class Machine:
+    indicator_lights: list[bool]
+    button_presses: list[tuple[int]]
+    joltages: tuple[int]
+     
 
 class Day10:
     def __init__(self,file_name:str) -> None:
         with open(file_name, 'r') as input_file:
-            pass
-            
+            lines = [line.strip('\n') for line in input_file]
+        
+        self._manual: list[Machine] = list()
+        for line in lines:    
+            parts: list[str] = line.split(' ')
+            indicator_lights: list[bool] = list(map(lambda l: l == '#',parts[0][1:-1]))
+            button_presses: list[tuple[int]] = list()
+            for buttons in parts[1:-1]:
+                 button_presses.append(tuple(map(lambda b: int(b),buttons[1:-1].split(','))))
+            joltages: tuple[int]=tuple(map(lambda j: int(j), parts[-1][1:-1].split(',')))
+            self._manual.append(Machine(indicator_lights,button_presses,joltages))
+    
+    def _min_presses(self, machine: Machine) -> int:
+        for num_combs in range(len(machine.button_presses)+1):
+            for presses in combinations(machine.button_presses,num_combs):
+                lights: list[bool] = [False] * len(machine.indicator_lights)
+                for press in presses:
+                    for button in press:
+                        lights[button] = not lights[button]
+                
+                # if lights match indicator lights - done
+                if lights == machine.indicator_lights:
+                    return num_combs
+           
     def part1(self) -> int:
-        return 0
+        result: int = 0
+        for machine in self._manual:
+            result += self._min_presses(machine)
+        return result
 
     def part2(self) -> int:
-        return 0
-                        
+        '''
+        Took a hint from reddit for this
+        Uses scipy linear programming to solve a system of linear equations
+        '''
+        result: float = 0
+        for machine in self._manual:
+            costs = [1 for b in machine.button_presses] 
+            eqs = [[i in b for b in machine.button_presses] for i in range(len(machine.joltages))]
+            result += linprog(costs, A_eq=eqs, b_eq=machine.joltages, integrality=1).fun
+        return int(result)                       
--- 2025/day10/tests/test_day10.py
+++ 2025/day10/tests/test_day10.py
@@ -1,18 +1,18 @@
-
+import os
 import pytest
-from Day10 import Day10
+from day10 import Day10
 
-test_data1=[('2025/Day10/tests/input.txt',0)]
-#test_data2=[('2025/Day10/tests/input.txt',0)]
+test_data1=[(os.path.dirname(__file__) + '/input.txt',7)]
+test_data2=[(os.path.dirname(__file__) + '/input.txt',33)]
                   
 @pytest.mark.parametrize('file_name,result',test_data1)
 def test_part1(file_name,result):
     d = Day10(file_name)
     assert(d.part1() == result)
 
-'''
+
 @pytest.mark.parametrize('file_name,result',test_data2)
 def test_part2(file_name,result):
     d = Day10(file_name)
     assert(d.part2() == result)
-'''
+
